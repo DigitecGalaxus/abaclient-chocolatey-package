2d52d15e37e633a0e85bff8a39b8a75e70a5e205
Beat Walti <BWalti@users.noreply.github.com>
2020-03-26 10:28:00 +0100

    added initial version

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,41 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$version = "2.1.600"
+$language = Get-WinSystemLocale | select -ExpandProperty Name | % { $_.substring(0,2) }
+$url = "https://storage.googleapis.com/images.abasky.net/119676df089a90ab3c3004398806433938052586dcd9a6bcacfb0e3e4024f339/$version/abaclient-$version-$language.msi"
+
+$packageArgs = @{
+  fullZipPath   = "$toolsDir\abaclient.msi"
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  file          = "$toolsDir\abaclient-$version-$language.msi"
+
+  softwareName  = "ABACUS AbaClient version $version"
+
+  checksum      = '3116777074DFEA05A428564ED1909329D1E21F65A21A7E34DC9F49F14E8E05E4'
+  checksumType  = 'sha256'
+
+  silentArgs    = "/quiet /passive /norestart /l `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Get-ChocolateyWebFile `
+  -PackageName $packageArgs.packageName `
+  -FileFullPath $packageArgs.file `
+  -Url $packageArgs.url
+
+Get-ChecksumValid `
+-File $packageArgs.file `
+-Checksum $packageArgs.checksum `
+-ChecksumType $packageArgs.checksumType `
+-OriginalUrl $packageArgs.url
+
+Install-ChocolateyInstallPackage `
+  -PackageName $packageArgs.packageName `
+  -FileType $packageArgs.fileType `
+  -File $packageArgs.file `
+  -SilentArgs $packageArgs.silentArgs `
+  -ValidExitCodes $packageArgs.validExitCodes
